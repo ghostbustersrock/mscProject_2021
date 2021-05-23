772b5103e23c1966ad0a32534801e55cee836b15
Luca Santarelli <luca.santarelli@hotmail.it>
2021-05-23 17:17:54 +0100

    Initial Commit

---
+++ mscProject_2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mscProject_2021
+//
+//  Created by Luca Santarelli on 23/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

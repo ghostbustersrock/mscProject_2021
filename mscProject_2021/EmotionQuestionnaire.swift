a3f4ad39065e14a5b8d524b2236c71b86454674d
Luca Santarelli <luca.santarelli@hotmail.it>
2021-07-05 18:28:28 +0200

    Added emotion detection ML model

---
+++ mscProject_2021/EmotionQuestionnaire.swift
@@ -0,0 +1,37 @@
+//
+//  EmotionQuestionnaire.swift
+//  mscProject_2021
+//
+//  Created by Luca Santarelli on 04/07/21.
+//
+
+import UIKit
+
+class EmotionQuestionnaire: UIViewController {
+    
+    @IBOutlet var displayQuestion:UILabel!
+    @IBOutlet var inputText:UITextField!
+    @IBOutlet var nextButton:UIButton!
+    
+    @IBAction func buttonFunc(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
